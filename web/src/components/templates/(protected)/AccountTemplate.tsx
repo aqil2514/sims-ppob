cb0a64576500a5bd475ecec920829fbcd26848be
Muhamad Aqil Maulana <aqilalumindo@gmail.com>
2025-07-31 13:41:14 +0700

    Loading

--- web/src/components/templates/(protected)/AccountTemplate.tsx
+++ web/src/components/templates/(protected)/AccountTemplate.tsx
@@ -10,7 +10,14 @@ export default function AccountTemplate() {
   const { data, isLoading } = useSWR("profile", () => getProfile(token!));
 
   if (isLoading || !data) {
-    return <MainContainer>Loading...</MainContainer>;
+    return (
+      <MainContainer className="flex justify-center items-center min-h-[300px]">
+        <div className="flex flex-col items-center">
+          <div className="animate-spin rounded-full h-10 w-10 border-4 border-t-transparent border-red-500 mb-2" />
+          <p className="text-sm text-gray-500">Memuat data profil...</p>
+        </div>
+      </MainContainer>
+    );
   }
 
   return (
